db7237a537c6e2ab000eeac47056ad06540633c4
armaanepiic <armaanepiicfb17@gmail.com>
2025-09-14 14:28:40 +0600

    Optima - TC : O(N), SC : O(1)

--- Array/268. Missing Number/Solution4.cpp
+++ Array/268. Missing Number/Solution4.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int xor1 = 0, xor2 = 0;
-        for(int i = 0 ; i <= n ; i++) {
-            xor1 = xor1 ^ i;
+        int XOR = 0;
+        for(int i = 0 ; i < n ; i++) {
+            XOR = XOR ^ nums[i] ^ i;
         }
-        for(int i = 0 ; i < nums.size() ; i++) {
-            xor2 = xor2 ^ nums[i];
-        }
-        return xor1 ^ xor2;
+        return XOR ^ n;
     }
 };
